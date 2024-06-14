14f64a3fe17c0bec455e205fc3700e399cd69776
Beck <iz596192@ucf.edu>
2024-06-14 16:06:08 -0400

    update

--- src/lex.c
+++ src/lex.c
@@ -2,176 +2,229 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include "lex.h"
 
-const char* reserved_words[] = {
-    "const", "var", "procedure", "call", "begin", "end", "if", "fi", "then", 
-    "else", "while", "do", "read", "write"
+#define MAX_IDENT_LEN 11
+#define MAX_NUM_LEN 5
+#define MAX_LEXEMES 1000
+
+typedef enum {
+    skipsym = 1, identsym, numbersym, plussym, minussym,
+    multsym, slashsym, fisym, eqlsym, neqsym, lessym, leqsym,
+    gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+    periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+    readsym, elsesym, errorsym
+} token_type;
+
+typedef struct {
+    char lexeme[MAX_IDENT_LEN + 1];
+    token_type token;
+    char error[50];
+} Lexeme;
+
+Lexeme lexemes[MAX_LEXEMES];
+int lexeme_count = 0;
+
+char *reservedWords[] = {
+    "const", "var", "procedure", "call", "begin", "end", "if", "fi",
+    "then", "else", "while", "do", "read", "write"
 };
 
-const token_type reserved_word_tokens[] = {
-    constsym, varsym, procsym, callsym, beginsym, endsym, ifsym, fisym, thensym, 
-    elsesym, whilesym, dosym, readsym, writesym
+token_type reservedTokens[] = {
+    constsym, varsym, procsym, callsym, beginsym, endsym, ifsym, fisym,
+    thensym, elsesym, whilesym, dosym, readsym, writesym
 };
 
-char* read_input(const char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        fprintf(stderr, "Error opening file %s\n", filename);
-        exit(1);
+void addLexeme(char *lex, token_type token, char *error) {
+    if (lexeme_count < MAX_LEXEMES) {
+        strcpy(lexemes[lexeme_count].lexeme, lex);
+        lexemes[lexeme_count].token = token;
+        if (error) {
+            strcpy(lexemes[lexeme_count].error, error);
+        } else {
+            lexemes[lexeme_count].error[0] = '\0';
+        }
+        lexeme_count++;
     }
-
-    char* buffer = malloc(MAX_INPUT_SIZE);
-    if (!buffer) {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(1);
-    }
-
-    size_t bytes_read = fread(buffer, 1, MAX_INPUT_SIZE, file);
-    buffer[bytes_read] = '\0';
-
-    fclose(file);
-    return buffer;
 }
 
-int is_letter(char c) {
-    return isalpha(c);
-}
-
-int is_digit(char c) {
-    return isdigit(c);
-}
-
-token_type get_reserved_word_token(const char* lexeme) {
-    for (size_t i = 0; i < sizeof(reserved_words) / sizeof(reserved_words[0]); i++) {
-        if (strcmp(lexeme, reserved_words[i]) == 0) {
-            return reserved_word_tokens[i];
+token_type identifyReservedWord(char *word) {
+    for (int i = 0; i < sizeof(reservedWords) / sizeof(reservedWords[0]); i++) {
+        if (strcmp(word, reservedWords[i]) == 0) {
+            return reservedTokens[i];
         }
     }
     return identsym;
 }
 
-void add_token(Lexeme lexemes[], Token tokens[], int* lexeme_count, int* token_count, const char* lexeme, token_type type, int error) {
-    strcpy(lexemes[*lexeme_count].lexeme, lexeme);
-    lexemes[*lexeme_count].type = type;
-    lexemes[*lexeme_count].error = error;
-    (*lexeme_count)++;
-    
-    if (!error) {
-        tokens[*token_count].type = type;
-        strcpy(tokens[*token_count].value, lexeme);
-        (*token_count)++;
+void printLexemeTable() {
+    printf("Lexeme Table:\n\n");
+    printf("%-20s%-10s\n", "lexeme", "token type");
+    for (int i = 0; i < lexeme_count; i++) {
+        printf("%-20s%-10d", lexemes[i].lexeme, lexemes[i].token);
+        if (lexemes[i].error[0] != '\0') {
+            printf(" Error: %s", lexemes[i].error);
+        }
+        printf("\n");
     }
 }
 
-void handle_improper_comment(const char* source, int start, int end, Lexeme lexemes[], Token tokens[], int* lexeme_count, int* token_count) {
-    add_token(lexemes, tokens, lexeme_count, token_count, "/", slashsym, 0);
-    add_token(lexemes, tokens, lexeme_count, token_count, "*", multsym, 0);
-    char buffer[256];
-    int idx = 0;
-    for (int j = start + 2; j < end; j++) {
-        if (isspace(source[j])) {
-            if (idx > 0) {
-                buffer[idx] = '\0';
-                add_token(lexemes, tokens, lexeme_count, token_count, buffer, identsym, 0);
-                idx = 0;
-            }
-        } else {
-            buffer[idx++] = source[j];
+void printTokenList() {
+    printf("Token List:\n");
+    for (int i = 0; i < lexeme_count; i++) {
+        printf("%d", lexemes[i].token);
+        if (lexemes[i].token == identsym || lexemes[i].token == numbersym) {
+            printf(" %s", lexemes[i].lexeme);
+        }
+        if (i < lexeme_count - 1) {
+            printf(" ");
         }
     }
-    if (idx > 0) {
-        buffer[idx] = '\0';
-        add_token(lexemes, tokens, lexeme_count, token_count, buffer, identsym, 0);
-    }
+    printf("\n");
 }
 
-void lexical_analysis(const char* source, Lexeme lexemes[], Token tokens[], int* lexeme_count, int* token_count) {
-    int i = 0;
-    int length = strlen(source);
+void processInput(const char *input) {
+    int length = strlen(input);
+    char buffer[MAX_IDENT_LEN + 1];
+    int buffer_index = 0;
 
-    while (i < length && *lexeme_count < MAX_LEXEMES && *token_count < MAX_TOKENS) {
-        if (isspace(source[i])) {
-            i++;
-        } else if (source[i] == '/' && source[i + 1] == '*') {
-            // Handle /* comments
-            int start = i;
-            i += 2;
-            int comment_closed = 0;
-            while (i < length) {
-                if (source[i] == '*' && source[i + 1] == '/') {
-                    comment_closed = 1;
-                    i += 2;
-                    break;
-                }
-                i++;
+    for (int i = 0; i < length; i++) {
+        char c = input[i];
+
+        if (isspace(c)) continue;  // Ignore whitespace
+
+        if (isalpha(c)) {  // Identifiers or reserved words
+            buffer_index = 0;
+            while (isalnum(c) && buffer_index < MAX_IDENT_LEN) {
+                buffer[buffer_index++] = c;
+                c = input[++i];
             }
-            if (!comment_closed) {
-                // Handle improperly enclosed comment
-                handle_improper_comment(source, start, i, lexemes, tokens, lexeme_count, token_count);
-                continue;
+            buffer[buffer_index] = '\0';
+            if (isalnum(c)) {  // Name too long
+                while (isalnum(c)) c = input[++i];
+                addLexeme(buffer, errorsym, "Name too long");
+            } else {
+                token_type token = identifyReservedWord(buffer);
+                addLexeme(buffer, token, NULL);
             }
-        } else if (source[i] == '/' && source[i + 1] == ' ') {
-            // Handle / * not a comment */
-            add_token(lexemes, tokens, lexeme_count, token_count, "/", slashsym, 0);
-            i++;
-        } else if (is_letter(source[i])) {
-            int start = i;
-            while (is_letter(source[i]) || is_digit(source[i])) i++;
-            int len = i - start;
-            char buffer[len + 1];
-            strncpy(buffer, &source[start], len);
-            buffer[len] = '\0';
-            token_type type = get_reserved_word_token(buffer);
-            add_token(lexemes, tokens, lexeme_count, token_count, buffer, type, len > MAX_LEXEME_LENGTH);
-        } else if (is_digit(source[i])) {
-            int start = i;
-            while (is_digit(source[i])) i++;
-            int len = i - start;
-            char buffer[len + 1];
-            strncpy(buffer, &source[start], len);
-            buffer[len] = '\0';
-            add_token(lexemes, tokens, lexeme_count, token_count, buffer, numbersym, len > MAX_NUMBER_LENGTH);
-        } else {
-            char buffer[3] = {0};
-            switch (source[i]) {
-                case '+': buffer[0] = '+'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, plussym, 0); break;
-                case '-': buffer[0] = '-'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, minussym, 0); break;
-                case '*': buffer[0] = '*'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, multsym, 0); break;
-                case '/': buffer[0] = '/'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, slashsym, 0); break;
-                case '(': buffer[0] = '('; add_token(lexemes, tokens, lexeme_count, token_count, buffer, lparentsym, 0); break;
-                case ')': buffer[0] = ')'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, rparentsym, 0); break;
-                case '=': buffer[0] = '='; add_token(lexemes, tokens, lexeme_count, token_count, buffer, eqlsym, 0); break;
-                case '<':
-                    if (source[i + 1] == '>') {
-                        buffer[0] = '<'; buffer[1] = '>'; buffer[2] = '\0'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, neqsym, 0); i++;
-                    } else if (source[i + 1] == '=') {
-                        buffer[0] = '<'; buffer[1] = '='; buffer[2] = '\0'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, leqsym, 0); i++;
+            i--;  // Adjust for extra increment
+        } else if (isdigit(c)) {  // Numbers
+            buffer_index = 0;
+            while (isdigit(c) && buffer_index < MAX_NUM_LEN) {
+                buffer[buffer_index++] = c;
+                c = input[++i];
+            }
+            buffer[buffer_index] = '\0';
+            if (isdigit(c)) {  // Number too long
+                while (isdigit(c)) c = input[++i];
+                addLexeme(buffer, errorsym, "Number too long");
+            } else {
+                addLexeme(buffer, numbersym, NULL);
+            }
+            i--;  // Adjust for extra increment
+        } else {  // Special symbols and errors
+            buffer[0] = c;
+            buffer[1] = '\0';
+            switch (c) {
+                case '+': addLexeme(buffer, plussym, NULL); break;
+                case '-': addLexeme(buffer, minussym, NULL); break;
+                case '*': addLexeme(buffer, multsym, NULL); break;
+                case '/':
+                    if (input[i + 1] == '*') {  // Start of comment
+                        i += 2;  // Skip '/*'
+                        while (i < length - 1 && !(input[i] == '*' && input[i + 1] == '/')) {
+                            i++;
+                        }
+                        if (i < length - 1) {
+                            i++;  // Skip '*/'
+                        } else {
+                            addLexeme("/*", errorsym, "Unterminated comment");
+                        }
                     } else {
-                        buffer[0] = '<'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, lessym, 0);
+                        addLexeme(buffer, slashsym, NULL);
+                    }
+                    break;
+                case '=': addLexeme(buffer, eqlsym, NULL); break;
+                case '<':
+                    if (input[i + 1] == '>') {
+                        buffer[1] = '>';
+                        buffer[2] = '\0';
+                        addLexeme(buffer, neqsym, NULL);
+                        i++;
+                    } else if (input[i + 1] == '=') {
+                        buffer[1] = '=';
+                        buffer[2] = '\0';
+                        addLexeme(buffer, leqsym, NULL);
+                        i++;
+                    } else {
+                        addLexeme(buffer, lessym, NULL);
                     }
                     break;
                 case '>':
-                    if (source[i + 1] == '=') {
-                        buffer[0] = '>'; buffer[1] = '='; buffer[2] = '\0'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, geqsym, 0); i++;
+                    if (input[i + 1] == '=') {
+                        buffer[1] = '=';
+                        buffer[2] = '\0';
+                        addLexeme(buffer, geqsym, NULL);
+                        i++;
                     } else {
-                        buffer[0] = '>'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, gtrsym, 0);
+                        addLexeme(buffer, gtrsym, NULL);
                     }
                     break;
-                case ';': buffer[0] = ';'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, semicolonsym, 0); break;
-                case '.': buffer[0] = '.'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, periodsym, 0); break;
-                case ',': buffer[0] = ','; add_token(lexemes, tokens, lexeme_count, token_count, buffer, commasym, 0); break;
                 case ':':
-                    if (source[i + 1] == '=') {
-                        buffer[0] = ':'; buffer[1] = '='; buffer[2] = '\0'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, becomessym, 0); i++;
+                    if (input[i + 1] == '=') {
+                        buffer[1] = '=';
+                        buffer[2] = '\0';
+                        addLexeme(buffer, becomessym, NULL);
+                        i++;
                     } else {
-                        buffer[0] = ':'; add_token(lexemes, tokens, lexeme_count, token_count, buffer, skipsym, 1);
+                        addLexeme(buffer, errorsym, "Invalid character");
                     }
                     break;
+                case ';': addLexeme(buffer, semicolonsym, NULL); break;
+                case ',': addLexeme(buffer, commasym, NULL); break;
+                case '.': addLexeme(buffer, periodsym, NULL); break;
+                case '(': addLexeme(buffer, lparentsym, NULL); break;
+                case ')': addLexeme(buffer, rparentsym, NULL); break;
                 default:
-                    buffer[0] = source[i]; add_token(lexemes, tokens, lexeme_count, token_count, buffer, skipsym, 1);
+                    addLexeme(buffer, errorsym, "Invalid character");
+                    break;
             }
-            i++;
         }
     }
+}
+
+void readInputFile(const char *filename, char *buffer, int size) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    int index = 0;
+    char c;
+    while ((c = fgetc(file)) != EOF && index < size - 1) {
+        buffer[index++] = c;
+    }
+    buffer[index] = '\0';
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const int bufferSize = 10000;
+    char input[bufferSize];
+
+    readInputFile(argv[1], input, bufferSize);
+
+    printf("Source Program:\n%s\n", input);
+
+    processInput(input);
+    printLexemeTable();
+    printTokenList();
+
+    return 0;
 }
